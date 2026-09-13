0cbb076abcc6e65127e75d589fe6e2698813b5a2
Zanvi chauhan <chauhanzanvi@gmail.com>
2026-09-13 19:38:59 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,66 @@
+
+import { useState } from 'react'
+import Arrow_fun from './arrowfun/Arrow_fun.jsx'
+import Constructor_fun from './constructorfun/Constructor_fun.jsx'
+import Recursive_fun from './recursivefun/Recursive_fun.jsx'
+import Async_fun from './asyncfun/Asyn_fun.jsx'
+import Object_Literals from './object_literals/Object_literals.jsx'
+import Object_Literals2 from './object_literals/Object_literals2.jsx'
+import Array_1 from './array/Array_1.jsx'
+import Array_2 from './array/Array_2.jsx'
+import Promises from './promises/Promises.jsx'
+import Primitive from './primitive/Primitive.jsx'
+import Non_primitive from './non_primitive/Non_primitive.jsx'
+import If from './conditional_statements/If.jsx'
+import If_else from './conditional_statements/If_else.jsx'
+import NestedIfElse from './conditional_statements/Nested_if_else.jsx'
+import Conditional_ternary_operator from './conditional_statements/Conditional_ternary_operator.jsx'
+import For_Loop from './looping_Statements/For_Loop.jsx'
+import For_in_loop from './looping_Statements/For_in_loop.jsx'
+import For_each from './looping_Statements/For_each.jsx'
+import Nested_for_loop from './looping_Statements/Nested_for_loop.jsx'
+import While_loop from './looping_Statements/While_loop.jsx'
+import Do_while_loop from './looping_Statements/Do_while_loop.jsx'
+import Switch_case from './looping_Statements/Switch_case.jsx'
+import Pure_fun from './purefun/Pure_fun.jsx'
+import Await_keyword from './await_keyword/Await_keyword.jsx'
+import Props from './props/Props.jsx'
+import Array_template_literals from './array_template_literals/Array_template_literals.jsx'
+import Object_literals_constructor from './object_literals_constructor/Object_literals_constructor.jsx'
+
+
+function App() {
+  return (
+    <div>
+       <Arrow_fun />
+       <Constructor_fun />
+       <Recursive_fun />
+       <Async_fun/>
+       <Object_Literals />
+       <Object_Literals2 />
+       <Array_1 />
+       <Array_2 />
+       <Promises/>
+       <Primitive/>
+       <Non_primitive/>
+       <If/>
+       <If_else/>
+       <NestedIfElse/>
+       <Conditional_ternary_operator/>
+       <For_Loop/>
+       <For_in_loop/>
+       <For_each/>
+       <Nested_for_loop/>
+       <While_loop/>
+       <Do_while_loop/>
+       <Switch_case/>
+       <Pure_fun/>
+       <Await_keyword/>
+       <Props/>
+       <Array_template_literals/>
+       <Object_literals_constructor/>
+    </div>
+  )
+}
+
+export default App;
